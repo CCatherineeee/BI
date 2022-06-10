0f2f4ee3d80536f958103e9504dcec6a08ac2e9f
CCatherinee <2870375520@qq.com>
2022-06-11 05:10:45 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // Paths
+    host: 'localhost',
+    port: '8080',
+    https: false,
+    open: true,
+    proxy: {
+      '/api': { // 匹配所有以 '/api'开头的请求路径
+        target: 'http://101.43.113.43:8080', // 代理目标的基础路径
+        changeOrigin: true, // 支持跨域
+        pathRewrite: { // 重写路径: 去掉路径中开头的'/api'
+          '^/api': ''
+        }
+      },
+    }
+  }
 })
